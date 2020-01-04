6ef81107be0cff9f4483c1adf65a58f92bf76070
chiajoukuo <b05901166@ntu.edu.tw>
2020-01-04 14:35:55 +0800

    v1

---
+++ strike_0.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
+
+try:
+    while True:
+        GPIO.output(17, True)
+        GPIO.output(18, False)
+
+except KeyboardInterrupt:
+    print('stop')
+finally:
+    GPIO.cleanup()
