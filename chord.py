1b073dfe0ef4563b129e1ef4c905202c32cd308c
Mike <mike132129@gmail.com>
2020-01-04 17:07:42 +0800

    mike

---
+++ chord.py
@@ -0,0 +1,50 @@
+# Motor1
+
+G1, G2 = 0, 50
+GB1, GB2 = 37.5, 50
+Fm71, Fm72 = 75, 125
+C1, C2 = 112.5, 87.5
+Am71, Am72 = 75, 87.5
+
+
+
+motor1 = [C1, GB1, Fm71, G1,
+		  C1, GB1, Fm71, G1, 
+		  C1, GB1, Fm71, G1, 
+		  C1, GB1, Fm71, G1,
+		  C1, Am71, Fm71, G1, 
+		  C1, Am71, Fm71, C1,
+		  C1, C1, Am71, Fm71,
+		  G1, C1, Am71, Fm71,
+		  C1, C1, C1, GB1,
+		  Fm71, G1, C1, GB1,
+		  Fm71, G1, Fm71, C1]
+
+motor2 = [C2, GB2, Fm72, G2,
+		  C2, GB2, Fm72, G2, 
+		  C2, GB2, Fm72, G2, 
+		  C2, GB2, Fm72, G2,
+		  C2, Am72, Fm72, G2, 
+		  C2, Am72, Fm72, C2,
+		  C2, C2, Am72, Fm72,
+		  G2, C2, Am72, Fm72,
+		  C2, C2, C2, GB2,
+		  Fm2, G2, C2, GB2,
+		  Fm72, G2, Fm72, C2]
+# Chord 
+'''
+C GB Fm7 G 
+C GB Fm7 G 前奏end
+
+C GB Fm7 G
+C GB Fm7 G
+C Am7 Fm7 G
+C Am7 (Fm7 G) C 
+C C Am7 Fm7 
+G C Am7 (Fm7 G) 
+C C C GB
+Fm7 G C GB 
+Fm7 G (Fm7 G) C
+
+
+'''
